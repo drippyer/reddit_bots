2da868d0dc4005da6202a3e1b94172c957bf3a77
drippyer <parker.capron@gmail.com>
2021-02-16 17:31:51 -0600

    Add AintStupid Bot

---
+++ AintStupid_Bot/README.md
@@ -0,0 +1,2 @@
+# AintStupid_Bot
+Small script that automatically replies "If it's stupid but it works, it ain't stupid!" on every submission to r/RedneckEngineering
---
+++ AintStupid_Bot/execute.py
@@ -0,0 +1,31 @@
+import random
+import praw
+
+sub = "Drip_Test"
+REPLIES = ["If it's stupid but it works, it ain't stupid!"]
+
+
+def main():
+
+    print("Connecting to reddit...")
+    reddit = praw.Reddit("stupid_bot", config_interpolation="basic")
+    subreddit = reddit.subreddit(sub)
+    print(f"Connected to: {subreddit.title}")
+
+    for submission in subreddit.stream.submissions():
+        process_submission(submission)
+
+
+def process_submission(submission):
+    if submission.saved is False:
+        print(f"Replying to: {submission.title}")
+        submission.reply(random.choice(REPLIES))
+        print("Replied!")
+
+        print(f"Saving: {submission.title}")
+        submission.save()
+        print("Saved!\n")
+
+
+if __name__ == "__main__":
+    main()
