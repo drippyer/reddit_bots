247139efd59c22c62a30609e09b870eba19f2cbf
drippyer <parker.capron@gmail.com>
2021-02-18 12:34:13 -0600

    apt-get -> apt

--- update_python.sh
+++ update_python.sh
@@ -23,7 +23,7 @@ echo "Installing Python $PY_VERSION"
 read -p "Press Enter to continue..."
 
 # Install dependencies to build
-sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev checkinstall
+sudo apt install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev checkinstall
 
 # Download Python tar
 echo "Now downloading Python $PY_VERSION"
